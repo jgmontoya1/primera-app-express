4734d6883daa8b306a9064bce19abefd5982eb35
jgmontoya1 <jgmontoya@utp.edu.co>
2020-07-30 12:37:33 -0500

    saludame 1

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+app.get('/user/:name', (req, res) => {
+  res.send(`<h1>hola ${req.params.name} !</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
