c1793121ea3e12ba4a75594e2bf1334fa8123ace
JUAN GUILLERMO MONTOYA <60018277+jgmontoya1@users.noreply.github.com>
2020-08-03 16:45:32 -0500

    saludame uno

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  let name = req.query.nombre;
-  if (!Object.keys(req.query).length){
-    name = "desconocido";
+  var nom = req.query.nombre
+  if (!(nom).length) {
+    nom = "desconocido"
   }
-  res.send("<h1> Hola " + name + "! </h1>");
+  res.send("<h1> Hola " + nom + "! </h1>");
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
