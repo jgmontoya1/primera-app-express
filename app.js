6e55c2f8b50e5241680dca26f4f97bbaca14ca92
JUAN GUILLERMO MONTOYA <60018277+jgmontoya1@users.noreply.github.com>
2020-08-13 14:38:26 -0500

    saludame 1 version 1.3

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// saludame 1 ver1.0
+// saludame 1 version 1.3
 var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
   var nom = req.query.nombre
   if (!(nom).length) {
-    nom = "Desconocido"
-  }res.end(`<h1>Hola ${nom}!</h1>`);
+    nom = "desconocido"
+  }res.send(`<h1>Hola ${nom}!</h1>`);
 });
 
 app.get('*', (req, res)=> {
-  res.end('<h1>Hola Desconocido!</h1>');
+  res.end('<h1>"Hola desconocido!"</h1>');
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!')); 
